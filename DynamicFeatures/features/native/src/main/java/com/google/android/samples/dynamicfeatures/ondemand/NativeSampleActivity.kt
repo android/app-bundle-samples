805f3d35902d023096a091974015708a14ac8039
Ben Weiss <benweiss@google.com>
2019-12-13 13:15:21 +0000

    Add guide to native code context renewal

--- DynamicFeatures/features/native/src/main/java/com/google/android/samples/dynamicfeatures/ondemand/NativeSampleActivity.kt
+++ DynamicFeatures/features/native/src/main/java/com/google/android/samples/dynamicfeatures/ondemand/NativeSampleActivity.kt
@@ -27,6 +27,12 @@ class NativeSampleActivity : BaseSplitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
+         If you want ot load a native library directly from the split install you'll have to create
+         a new context by calling `createPackageContext(packageName, 0)` and pass it into the
+         `loadLibrary` call like outlined here:
+         https://developer.android.com/guide/app-bundle/playcore#load_native_libs
+         */
         SplitInstallHelper.loadLibrary(this, "hello-jni")
 
         setContentView(R.layout.activity_hello_jni)
