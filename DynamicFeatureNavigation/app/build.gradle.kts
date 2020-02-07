73978510e81a28b2c27659f7db1b1848d449cf61
Ben Weiss <benweiss@google.com>
2020-02-07 09:27:21 -0800

    Add DynamicFeatureNavigation sample

---
+++ DynamicFeatureNavigation/app/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        applicationId = "com.google.android.samples.dynamicnavigator"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 3
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    dynamicFeatures = mutableSetOf(":dynamicfeature", ":includedgraphfeature")
+
+    packagingOptions {
+        exclude("META-INF/**.version")
+    }
+
+}
+
+dependencies {
+    api("androidx.navigation:navigation-dynamic-features-fragment:2.3.0-alpha01")
+    api("androidx.appcompat:appcompat:1.1.0")
+    api("androidx.constraintlayout:constraintlayout:1.1.3")
+}
