d279998220a1ca9c188c56f2fd1947a0fc95c745
Ben Weiss <benweiss@google.com>
2021-01-08 11:48:31 +0000

    Update build dependencies to stable versions

--- PlayCoreKtx/buildSrc/build.gradle.kts
+++ PlayCoreKtx/buildSrc/build.gradle.kts
@@ -26,8 +26,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle-api:4.1.0-rc03")
-    implementation("com.android.tools.build:gradle:4.1.0-rc03")
+    implementation("com.android.tools.build:gradle-api:4.1.1")
+    implementation("com.android.tools.build:gradle:4.1.1")
     implementation(kotlin("stdlib"))
     gradleApi()
 }
